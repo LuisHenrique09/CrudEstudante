605f5a48da1768e4ebc5e627eed2bba943b54853
LuisHenrique <luishenriquecorporativo@gamil.com>
2023-06-13 23:56:26 -0300

    ComponentePaieFilho

---
+++ src/app/estudantes-form/estudantes-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estudantes-form',
+  templateUrl: './estudantes-form.component.html',
+  styleUrls: ['./estudantes-form.component.css']
+})
+export class EstudantesFormComponent {
+
+}
